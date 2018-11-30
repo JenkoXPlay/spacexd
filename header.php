19f17597d13baedf07d2988a0b8fa9a4e005e07d
Bastien_mx <bastienmoiroux@gmail.com>
2018-11-30 11:57:25 +0100

    ajout de la page mars mobile

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Space XD</title>
     <link rel="stylesheet" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <script type="text/javascript" src="js/jquery.min.js"></script>
 </head>
 <body>
@@ -28,7 +29,7 @@
             <input type="text" name="search" placeholder="Recherche" />
             <a href="./home.php">Home</a>
             <a href="">What we do</a>
-            <a href="">Project_mars</a>
+            <a href="./mars.php">Project_mars</a>
             <a href="">Events</a>
             <a href="">Contact</a>
         </div>
