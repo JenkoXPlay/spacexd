dea726e8f94bbe9b29877f42c2f6347b90edeb5e
Maxime <maxime.lefebvre-isc.france@ibm.com>
2019-01-05 14:27:23 +0100

    Ajout du design de la page mars (pc/mobile)

--- mars.php
+++ mars.php
@@ -1,38 +1,57 @@
 <?php include('header.php'); ?>
 
-    <div class="bgGlobal">
-        <div class="contentGlobal">
-            
-            <h3 class="title">Project_Mars</h3>
+<div class="bgGlobal">
+	<div class="contentGlobal">
 
-            <div class="text text_mars">
-                <p class=text_title>Why this project ?</p>
-                <p class=text_content_mars>
-                    "The population on earth grows larger everyday, 
-                    and it's time to prepare ourselves to migrate to another land.
-                    Project_Mars gives new hopes for human salvation, 
-                    and opens a new path towards life on Mars."
-                </p>
-                <p class=text_sign_mars>- SpaceXD</p>
-            </div>
-        
-            <div class="ticket_mars">
+		<h3 class="title">PROJECT_MARS</h3>
 
-                <div class="text_ticket_mars">
-                    <p class="title_ticket_mars">TICKET FOR MARS</p>
+		<section class="mars-flex">
 
-                    <a class="button_ticket_mars">
-                        BUY NOW
-                    </a>
-                </div>
-                
-                <!-- <div class="img_ticket_mars">      -->
-                    <img class="img_ticket_mars" src="img/logo_icon.svg">
-                <!-- </div>   -->
+			<img class="ticket_image--img" src="img/ticket-mars.png">
+			<img class="ticket_image--mobileimg" src="img/ticket-mars-mobile.png">
 
-            </div>
+			<!-- TICKET 
+		<div class="ticket_mars">
+			<div class="text_ticket_mars">
+				<span class="title_ticket_mars">TICKET FOR MARS</span>
+				<a class="button_ticket_mars">BUY NOW</a>
+			</div>
+			<img class="img_ticket_mars" src="img/logo_icon.svg">
+		</div>-->
 
-        </div>
-    </div>
+			<!-- BOTTOM FLEX -->
+			<div class="mars_bottom_image">
+				<!-- WHY THIS PROJECT -->
+				<div class="text text_mars">
+					<p class=text_title>Why this project ?</p>
+					<p class=text_content_mars>
+						"The population on earth grows larger everyday,
+						and it's time to prepare ourselves to migrate to another land.
+						Project_Mars gives new hopes for human salvation,
+						and opens a new path towards life on Mars."
+					</p>
+					<span class=text_sign_mars>- SpaceXD</span>
+
+				</div>
+				<img class="robot_image--img" src="img/robot-mars.png">
+			</div>
+		</section>
+		<!-- RIGHT PART-->
+		<div class="mars_right">
+			<img class="mars_image--img" src="img/mars-planet.png">
+			<ul>
+				<li><span> Gravity :</span> 3,711 m/s2; (0,379 g) </li>
+				<li><span> Molat mass :</span> 43,34 g/mol </li>
+				<li><span> Ray :</span> 3 390 km </li>
+				<li><span> Distance from the sun :</span> 227,9 millions km </li>
+				<li><span> Temperature :</span> -63°C </li>
+				<li><span> Inhabitant :</span> 0 </li>
+				<li><span> Water vapeur :</span> 0,03% </li>
+				<li><span> Know stellites :</span> Phobos and Démios</li>
+			</ul>
+		</div>
+
+	</div>
+</div>
 
 <?php include('footer.php'); ?>
